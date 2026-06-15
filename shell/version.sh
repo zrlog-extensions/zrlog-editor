0bc255fc865f3c49d2b8c7873e90de6545196bb8
xiaochun <90xchun@gmail.com>
2026-06-15 22:24:56 +0800

    release by shell[2.1.26]

--- shell/version.sh
+++ shell/version.sh
@@ -2,21 +2,31 @@
 baseVersion=2.1
 releaseVersion=${baseVersion}.${1}
 nextVersion=${baseVersion}.$((${1}+1))-SNAPSHOT
+writeEditorVersion() {
+  printf 'export const editorVersion = "%s";\n' "$1" > src/editor/editor-version.ts
+}
 # do lint
 rm -rf temp
 mkdir -p artifacts
 rm -f artifacts/v${releaseVersion}.tgz
 yarn lint
-yarn version --new-version=${releaseVersion}
+yarn version --new-version=${releaseVersion} --no-git-tag-version
+writeEditorVersion "${releaseVersion}"
+git add package.json yarn.lock src/editor/editor-version.ts
+git commit -m"v${releaseVersion}"
+git tag "v${releaseVersion}"
 mkdir -p temp && cp README.md temp && cp -R src temp && cp package.json temp && cp tsconfig.json temp/tsconfig.json && cp tsconfig-pack.json temp/tsconfig-pack.json && cp .eslintrc.json temp
 rm -rf temp/src/pages
 rm -rf temp/src/index.tsx
 rm -rf temp/src/react-app-env.d.ts
-printf 'export const editorVersion = "%s";\n' "${releaseVersion}" > temp/src/editor/editor-version.ts
 cd temp && tsc --project tsconfig-pack.json --incremental false && yarn pack --filename v${releaseVersion}.tgz
 cd ..
 mv temp/*.tgz artifacts && rm -rf temp
 git add artifacts shell src public package.json yarn.lock README.md build
 git commit -m"release by shell[${releaseVersion}]"
-yarn version --new-version=${nextVersion}
+yarn version --new-version=${nextVersion} --no-git-tag-version
+writeEditorVersion "${nextVersion}"
+git add package.json yarn.lock src/editor/editor-version.ts
+git commit -m"v${nextVersion}"
+git tag "v${nextVersion}"
 git push
